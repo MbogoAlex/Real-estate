d8f8e61166ad1b97bad3bc5455701dbee214b15e
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-03-15 16:02:31 +0300

    splash screen

--- app/src/main/java/com/example/propertymanagement/MainActivity.kt
+++ app/src/main/java/com/example/propertymanagement/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.propertymanagement
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.propertymanagement.ui.PManagerApp
 import com.example.propertymanagement.ui.theme.PropertyManagementTheme
 import com.example.propertymanagement.ui.views.AvailabilitySelection
@@ -20,15 +23,16 @@ import com.example.propertymanagement.ui.views.UnitsScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             PropertyManagementTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
-//                    PManagerApp()
-                    CreateNewPropertyScreen()
+                    PManagerApp()
+//                    CreateNewPropertyScreen()
                 }
             }
         }
